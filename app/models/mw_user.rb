12d8f7fce2d17e2ec69b157acd6e6218b98a5ee7
miaaaooow <mateva.maria@gmail.com>
2012-02-14 16:29:47 +0200

    Delete extra files.

--- app/models/mw_user.rb
+++
@@ -1,9 +0,0 @@
-class MWUser < ActiveRecord::Base
-  validates :birthday, :birthdate => true
-  
-  belongs_to :user
-  
-  # multiple foreign keys to Relationship
-  has_many :from_relationships, :class_name => Relationship, :foreign_key => :from_mw_user_id
-  has_many :to_relationships, :class_name => Relationship, :foreign_key => :to_mw_user_id
-end
